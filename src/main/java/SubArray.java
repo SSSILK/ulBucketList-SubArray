dd5d72871f1b96ac56a9c1e30956020ddd690be8
Daijohn Bryant <ohnntsendto@gmail.com>
2025-07-18 16:43:23 +0000

    Lab --Completed

--- src/main/java/SubArray.java
+++ src/main/java/SubArray.java
@@ -13,6 +13,11 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+        int [] array = new int[end-start];
+        for(int i = 0; i < array.length; i++){
+            array[i] = nums[start + i];}
+       
+        
+        return array;
     }
 }
